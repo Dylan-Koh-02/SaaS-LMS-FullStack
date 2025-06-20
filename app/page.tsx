f0cf2af5933c49e27d2aad19845802aaabb81f65
Dylan <zikangkoh0113@gmail.com>
2025-06-20 21:30:37 +0800

    Main Page Implementation (20250620)

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,46 @@
-/* eslint-disable react/no-unescaped-entities */
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import { recentSessions } from "@/constants";
+// import { Button } from '@/components/ui/button'
+import React from "react";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to my SaaS App!</h1>
-      <Button>
-        Let's get started!
-      </Button>
-    </div>
-  )
-}
+    <main>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="1"
+          name="Learn Web Fundamentals"
+          topic="HTML5/CSS3"
+          subject="Computer Science / Information Technology"
+          duration={60}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="2"
+          name="Deep Dive into Modern Web"
+          topic="React 19"
+          subject="Computer Science / Information Technology"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="3"
+          name="Behind the Scene"
+          topic="Node.js"
+          subject="Computer Science / Information Technology"
+          duration={30}
+          color="#DBE7FF"
+        />
+      </section>
+      <section className="home-section">
+        <CompanionsList title='Recently Completed Sessions' companions={recentSessions} classNames='w-2/3 max-lg:w-full'/>
+        <CTA />
+      </section>
+    </main>
+  );
+};
 
-export default Page
+export default Page;
